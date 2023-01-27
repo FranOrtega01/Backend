9f9009891e8fc32de91df44080fadd90fab638cd
FranOrtega01 <104398010+FranOrtega01@users.noreply.github.com>
2023-01-27 17:40:20 -0300

    Delete clase05 directory

--- clase05/script.js
+++
@@ -1,9 +0,0 @@
-// const obj = {}
-
-// for(let i = 0; i <= 10000; i++){
-//     const randomNumber =  Math.floor(Math.random() * 20);
-//     // if(!obj[randomNumber]) obj[randomNumber] = 1
-//     // else obj[randomNumber]++
-//     !obj[randomNumber] ? obj[randomNumber] = 1 : obj[randomNumber]++
-// }
-// console.log(obj);
